f250b3f668f07c4eb80ed29717ef6dca7b54503a
hiroyuki-seto <hiswing8@gmail.com>
2022-05-14 22:37:12 +0900

    clientのURLを外部ファイルに移した

--- src/main/kotlin/karate/sample/hiswinglabo/infra/gateway/CatFactClient.kt
+++ src/main/kotlin/karate/sample/hiswinglabo/infra/gateway/CatFactClient.kt
@@ -3,7 +3,7 @@ package karate.sample.hiswinglabo.infra.gateway
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.GetMapping
 
-@FeignClient(name = "cats", url = "https://cat-fact.herokuapp.com")
+@FeignClient(name = "cats", url = "\${catfat.url}")
 interface CatFactClient {
     @GetMapping("/facts")
     fun fetch(): String
