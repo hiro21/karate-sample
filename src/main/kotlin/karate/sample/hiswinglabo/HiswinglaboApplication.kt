39292695494d61381ab70139ff456fc6735cb583
hiroyuki-seto <hiswing8@gmail.com>
2022-05-14 13:16:09 +0900

    first commit

---
+++ src/main/kotlin/karate/sample/hiswinglabo/HiswinglaboApplication.kt
@@ -0,0 +1,11 @@
+package karate.sample.hiswinglabo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HiswinglaboApplication
+
+fun main(args: Array<String>) {
+	runApplication<HiswinglaboApplication>(*args)
+}
---
+++ src/test/kotlin/karate/sample/hiswinglabo/HiswinglaboApplicationTests.kt
@@ -0,0 +1,13 @@
+package karate.sample.hiswinglabo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HiswinglaboApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
