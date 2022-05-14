30eff49a9a37872ef95bda0f16146f5ecf8259c4
hiroyuki-seto <hiswing8@gmail.com>
2022-05-14 13:21:35 +0900

    add dependency karate

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("com.intuit.karate:karate-junit5:1.2.0")
 }
 
 dependencyManagement {
