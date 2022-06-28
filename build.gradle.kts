2ea41f82bc51b1e4d5a5885054042d6ce32ebcf1
hiroyuki-seto <hiswing8@gmail.com>
2022-06-28 21:10:27 +0900

    add jig

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "2.6.7"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
+	id("org.dddjava.jig-gradle-plugin") version "2022.6.3"
 	kotlin("jvm") version "1.6.21"
 	kotlin("plugin.spring") version "1.6.21"
 }
